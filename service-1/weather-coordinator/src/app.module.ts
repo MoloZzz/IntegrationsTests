4cdb62b5d05b5d265bc0815f30792e10dda70f9f
MoloZzz <mega_oleksii@ukr.net>
2025-03-21 00:47:25 +0200

    feat: added integrations directory and first module

--- service-1/weather-coordinator/src/app.module.ts
+++ service-1/weather-coordinator/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { WeatherModule } from './weather/weather.module';
+import { OpenWeatherModule } from './integrations/open-weather/open-weather.module';
 
 @Module({
-  imports: [WeatherModule],
+  imports: [WeatherModule, OpenWeatherModule],
   controllers: [AppController],
   providers: [AppService],
 })
