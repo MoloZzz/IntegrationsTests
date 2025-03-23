3c001b3db631ee97d25ae20610c599707ba9e247
MoloZzz <mega_oleksii@ukr.net>
2025-03-23 23:45:14 +0200

    feat: init integrations modules(empty)

--- service-5/notificator/src/app.module.ts
+++ service-5/notificator/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TelegramModule } from './integrations/telegram/telegram.module';
+import { EmailModule } from './integrations/email/email.module';
 
 @Module({
-  imports: [],
+  imports: [TelegramModule, EmailModule],
   controllers: [AppController],
   providers: [AppService],
 })
